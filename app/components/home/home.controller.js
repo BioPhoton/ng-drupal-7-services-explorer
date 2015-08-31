9b01b53e4be3ebda52e7c6f417832b4b49b17b84
Michael Hladky <michael@hladky.at>
2015-08-31 23:57:18 +0200

    test auth files

--- app/components/home/home.controller.js
+++ app/components/home/home.controller.js
@@ -3,15 +3,36 @@
 
 
 angular
-    .module('ngDrupalServicesTests.home.controller', ['ngDrupal7Services-3_x.resources.system.resource', 'ngDrupal7Services-3_x.resources.system.channel', 'ngDrupal7Services-3_x.commons.baseChannel'])
+    .module('ngDrupalServicesTests.home.controller', [
+                                                     'ngDrupal7Services-3_x.commons.http.intercepter.requestAccept'
+                                                    ,'ngDrupal7Services-3_x.commons.authentication.channelConstant'
+                                                    ,'ngDrupal7Services-3_x.commons.authentication.channel'
+                                                    ,'ngDrupal7Services-3_x.commons.authentication.serviceConstant'
+                                                    //,'ngDrupal7Services-3_x.commons.authentication.service'
+                                                    ])
     .controller('HomeController', HomeController);
 
-	HomeController.$inject = ['$scope', 'SystemResource', 'SystemChannel', 'BaseChannel'];
+	HomeController.$inject = ['$scope', 
+	                          'RequestIntercepterAccept'
+	                          ,'AuthenticationChannelConstant'
+	                          ,'AuthenticationChannel',
+	                          //,'AuthenticationServiceConstant'
+	                          //,'AuthenticationService'
+	                          ];
 
 	/** @ngInject */ 
-	function HomeController($scope, SystemResource, SystemChannel, BaseChannel) { 
-		console.log('BaseChannel: ', BaseChannel);
-		console.log('SystemChannel: ', SystemChannel);
+	function HomeController($scope,  RequestIntercepterAccept
+									,AuthenticationChannelConstant
+									,AuthenticationChannel
+									//,AuthenticationServiceConstant
+									//,AuthenticationService
+			) { 
+		console.log('RequestIntercepterAccept: ', RequestIntercepterAccept); 
+		console.log('AuthenticationChannelConstant: ', AuthenticationChannelConstant); 
+		console.log('AuthenticationChannel: ', AuthenticationChannel); 
+		//console.log('AuthenticationServiceConstant: ', AuthenticationServiceConstant);
+		//console.log('AuthenticationService: ', AuthenticationService);
+		
 		var requestEnd = 0;
 		var requestStart = 0;
 		 
@@ -24,63 +45,7 @@ angular
 		vm.connectRequests = [];
 		//test request
 		vm.doConncet = doConncet;
-		//test the connect on confirm event
-		SystemChannel.subSystemConnectConfirmed($scope, subSystemConnectConfirmedCallback);
-		//test the connect on failed event
-	    //SystemChannel.subSystemConnectFailed($scope, subSystemConnectFailedCallback);
-	    
-	    //__________________________________________________________________________________________________
-	    /*
-	    //get_variable request
 		
-	    //store requests
-		vm.getVariableRequests = [];
-		//test request
-		vm.doGetVariable = doGetVariable;
-		vm.getVariableData = {
-				   name : '',
-				   default : ''
-		};
-		//test the connect on confirm event
-		SystemChannel.subSystemGetVariableConfirmed($scope, subSystemGetVariableConfirmedCallback);
-		//test the connect on failed event
-		SystemChannel.subSystemGetVariableFailed($scope, subSystemGetVariableFailedCallback);
-
-	    //__________________________________________________________________________________________________
-	    
-		//set_variable request
-		
-		//store requests
-		vm.setVariableRequests = [];
-		//test request
-		vm.doSetVariable = doSetVariable;
-		vm.setVariableData = {
-				   name 	: '',
-				   value 	: ''
-		};
-		//test the connect on confirm event
-		SystemChannel.subSystemSetVariableConfirmed($scope, subSystemSetVariableConfirmedCallback);
-		//test the connect on failed event
-		SystemChannel.subSystemSetVariableFailed($scope, subSystemSetVariableFailedCallback);
-
-	    //__________________________________________________________________________________________________
-		
-		//del_variable request
-		
-		//store requests
-		vm.delVariableRequests = [];
-		//test request
-		vm.doDelVariable = doDelVariable;
-		vm.delVariableData = {
-				   name 	: ''
-		};
-		//test the connect on confirm event
-		SystemChannel.subSystemDelVariableConfirmed($scope, subSystemDelVariableConfirmedCallback);
-		//test the connect on failed event
-		SystemChannel.subSystemDelVariableFailed($scope, subSystemDelVariableFailedCallback);
-	*/
-	    //__________________________________________________________________________________________________
-	    
 		///////////////////////
 		
 		//connect request
@@ -96,105 +61,7 @@ angular
 			    		function(data) { console.log('system conncet error'); }
 			    );
 		}
-		//confirm callback
-		function subSystemConnectConfirmedCallback(data) { 
-			requestEnd = Date.now();
-			console.log('subSystemConnectConfirmed'); 
-			vm.connectRequests.push({requestStart:requestStart, requestEnd:requestEnd, requestDuration:requestEnd-requestStart, data:data});
-		}
-		//failed callback
-		function subSystemConnectFailedCallback(data) { 
-			requestEnd = Date.now();
-			console.log('subSystemConnectFailed'); 
-			vm.connectRequests.push({requestStart:requestStart, requestEnd:requestEnd,  requestDuration:requestEnd-requestStart, data:data});
-		}
 		
-		//_____________________________________________________________________________________________________________________________________________
-		/*
-		//get_variable request
-		
-		//do request
-		function doGetVariable() {
-			requestStart = Date.now();
-				SystemResource.get_variable(vm.getVariableData)
-					.then(
-				    	//get_variable success
-				    	function(data) { console.log('system get_variable success'); },
-				    	//get_variable error
-				    	function(data) { console.log('system get_variable error'); }
-				    );
-		};
-		//confirm callback
-		function subSystemGetVariableConfirmedCallback(data) { 
-		   requestEnd = Date.now();
-		   console.log('subSystemGetVariableConfirmed'); 
-		   vm.getVariableRequests.push({requestStart:requestStart, requestEnd:requestEnd,  requestDuration:requestEnd-requestStart, data:data});
-		};
-		//failed callback
-		function subSystemGetVariableFailedCallback(data) { 
-		   requestEnd = Date.now();
-		   console.log('subSystemGetVariableFailed');
-		   vm.getVariableRequests.push({requestStart:requestStart, requestEnd:requestEnd,  requestDuration:requestEnd-requestStart, data:data});
-	   };
-		
-	   //_____________________________________________________________________________________________________________________________________________
-	   
-	   //set_variable request
-		
-		//do request
-		function doSetVariable() {
-			requestStart = Date.now();
-				SystemResource.set_variable(vm.setVariableData)
-					.then(
-				    	//get_variable success
-				    	function(data) { console.log('system set_variable success'); },
-				    	//get_variable error
-				    	function(data) { console.log('system set_variable error'); }
-				    );
-		};
-		//confirm callback
-		function subSystemSetVariableConfirmedCallback(data) { 
-		   requestEnd = Date.now();
-		   console.log('subSystemSetVariableConfirmed'); 
-		   vm.setVariableRequests.push({requestStart:requestStart, requestEnd:requestEnd,  requestDuration:requestEnd-requestStart, data:data});
-		};
-		//failed callback
-		function subSystemSetVariableFailedCallback(data) { 
-		   requestEnd = Date.now();
-		   console.log('subSystemSetVariableFailed');
-		   vm.setVariableRequests.push({requestStart:requestStart, requestEnd:requestEnd,  requestDuration:requestEnd-requestStart, data:data});
-		};
-		   
-		//_____________________________________________________________________________________________________________________________________________
-		  
-		//del_variable request
-		
-		//do request
-		function doDelVariable() {
-			requestStart = Date.now();
-				SystemResource.del_variable(vm.delVariableData)
-					.then(
-				    	//get_variable success
-				    	function(data) { console.log('system del_variable success'); },
-				    	//get_variable error
-				    	function(data) { console.log('system del_variable error'); }
-				    );
-		};
-		//confirm callback
-		function subSystemDelVariableConfirmedCallback(data) { 
-		   requestEnd = Date.now();
-		   console.log('subSystemDelVariableConfirmed'); 
-		   vm.delVariableRequests.push({requestStart:requestStart, requestEnd:requestEnd,  requestDuration:requestEnd-requestStart, data:data});
-		};
-		//failed callback
-		function subSystemDelVariableFailedCallback(data) { 
-		   requestEnd = Date.now();
-		   console.log('subSystemDelVariableFailed');
-		   vm.delVariableRequests.push({requestStart:requestStart, requestEnd:requestEnd,  requestDuration:requestEnd-requestStart, data:data});
-		};
-		 */
-		//_____________________________________________________________________________________________________________________________________________
-
 	};
 	
 	
