683c8b62e2476138c61bb2658798ec92910a6644
Michael Hladky <michael@hladky.at>
2015-10-23 03:44:22 +0200

    extende config and states

--- app/app.config.js
+++ app/app.config.js
@@ -6,14 +6,19 @@ angular
     .module('ngDrupalServicesTests.config', ['ngDrupal7Services-3_x.commons.configurations','ngDrupal7Services-3_x.commons.http.configurations'])
     .config(configFunction);
 
-	configFunction.$inject = ['DrupalApiConstant'];
+	configFunction.$inject = ['DrupalApiConstant', '$ionicLoadingConfig'];
 
 	/** @ngInject */ 
-	function configFunction(DrupalApiConstant) 
+	function configFunction(DrupalApiConstant, $ionicLoadingConfig) 
 	{ 
 		//drupal services configurations
 		DrupalApiConstant.drupal_instance = 'http://www.drupalionic.org/drupal_test/';
 		DrupalApiConstant.api_endpoint += 'v1/';
+		
+		//Configure loading intercepter
+		//http://ionicframework.com/docs/api/service/$ionicLoading/
+		$ionicLoadingConfig.template = '<p><ion-spinner></ion-spinner><br/>Loading...</p>';
+		
 		
 	};
 
